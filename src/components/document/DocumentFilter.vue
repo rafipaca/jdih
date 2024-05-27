2ce7dfa415e2a5396c66f3ca6483a76cb5ba95d7
Rafi Rizha Syakhari - 222112299 <222112299@stis.ac.id>
2024-05-27 14:12:29 +0700

    Memperbaiki DocumentPage beta

--- src/components/document/DocumentFilter.vue
+++ src/components/document/DocumentFilter.vue
@@ -21,27 +21,38 @@ export default {
 </style> -->
 
 <!-- Ini adalah batas -->
+<style>
+  .placeholder-center {
+    text-align: center;
+  }
+</style>
 
 <template>
-  <div class="mb-4">
+  <div class="mb-4 w-full">
     <h2 class="text-2xl font-bold">Cari dokumen hukum!</h2>
-    <div class="flex space-x-4 mt-4">
-      <div class="relative flex items-center">
-        <!-- Tambahkan class relative dan flex di sini -->
-        <img src="@/assets/book.png" alt="Icon" class="absolute left-0 w-5 h-5" />
-        <input type="text" placeholder="Judul" class="input input-bordered pl-8 w-full max-w-xs" />
+    <div class="flex space-x-2 mt-4 w-full">
+      <!-- Bagian pertama untuk input "Judul" -->
+      <div class="relative flex items-center w-1/2">
+        <img src="@/assets/book.png" alt="Icon" class="absolute left-0 w-5 h-5 ml-4" />
+        <input type="text" placeholder="Judul" class="input shadow-lg input-bordered pl-12 w-full placeholder-center" />
       </div>
-      <div class="relative flex items-center">
-        <!-- Tambahkan class relative dan flex di sini -->
-        <img src="@/assets/neraca.png" alt="Icon" class="absolute left-0 w-5 h-5" />
-        <input type="text" placeholder="Nomor" class="input input-bordered pl-8 w-full max-w-xs" />
+      <!-- Bagian kedua untuk input "Nomor", "Tahun", dan button -->
+      <div class="flex space-x-2 w-1/2">
+        <!-- Input "Nomor" -->
+        <div class="relative flex items-center w-1/3">
+          <img src="@/assets/neraca.png" alt="Icon" class="absolute left-0 w-5 h-5 ml-4" />
+          <input type="text" placeholder="Nomor" class="input shadow-lg input-bordered pl-12 w-full placeholder-center " />
+        </div>
+        <!-- Input "Tahun" -->
+        <div class="relative flex items-center w-1/3">
+          <img src="@/assets/calendar.png" alt="Icon" class="absolute left-0 w-5 h-5 ml-4" />
+          <input type="text" placeholder="Tahun" class="input shadow-lg input-bordered pl-12 w-full placeholder-center" />
+        </div>
+        <!-- Button -->
+        <div class="flex justify-items-end justify-end w-1/3">
+          <button class="btn w-full text-slate-50 bg-[#ffc067] shadow-lg">Temukan!</button>
+        </div>
       </div>
-      <div class="relative flex items-center">
-        <!-- Tambahkan class relative dan flex di sini -->
-        <img src="@/assets/calendar.png" alt="Icon" class="absolute left-0 w-5 h-5" />
-        <input type="text" placeholder="Tahun" class="input input-bordered pl-8 w-full max-w-xs" />
-      </div>
-      <button class="btn btn-primary">Temukan!</button>
     </div>
   </div>
 </template>
