89532f653249e004bc8e4f7601fceacc218065b6
Naufal Ihsan Putra Marlin <222112246@stis.ac.id>
2024-05-29 21:20:55 +0700

    judul about view

--- src/components/about/VisiMisi.vue
+++ src/components/about/VisiMisi.vue
@@ -1,6 +1,4 @@
 <template>
-    <h1 class="text-4xl font-bold">Tentang Kami</h1>
-      <div class="border-t-4 border-teal-500 w-24"></div>
     <div class="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-lg">
       <h2 class="text-2xl font-semibold text-center text-teal-500">Visi</h2>
       <p class="italic text-center">“Lampung Maju dan Sejahtera Tahun 2019”</p>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,20 @@
 <template>
+  <div class="text-4xl text-center mt-10 font-bold">Tentang Kami</div>
+      <div class="border-t-4 border-teal-500 w-60 mx-auto mt-2"></div>
   <section class="flex flex-col lg:flex-row items-start justify-center py-8 px-4">
+    <div class="text-center mb-8">
+    </div>
+    <div class="flex flex-col lg:flex-row items-start justify-center w-full gap-4">
     <aside class="w-full lg:w-1/4 p-4">
       <Sidebar />
     </aside>
-    <main class="flex-1 p-4">
+    <main class="w-full lg:w-1/4 p-4">
       <VisiMisi />
     </main>
     <aside class="w-full lg:w-1/4 p-4">
       <DasarHukum />
     </aside>
+  </div>
   </section>
 </template>
 
