4fc6d9ffd49cfabc1cf8dc325a28c314f713cfaa
Yoga Andika <222112102@stis.ac.id>
2024-06-16 16:05:53 +0700

    Tambahin breadcrumbs dan dekorasi

---
+++ src/components/feed/DecorFeed.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <div class="decor-top"></div>
+      <div class="content-container flex flex-col items-center min-h-screen pb-5">
+        <slot></slot>
+      </div>
+      <div class="decor-bottom"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DecorFeed'
+  }
+  </script>
+  
+  <style scoped>
+  .decor-top,
+  .decor-bottom {
+    position: static;
+    left: 0;
+    width: 100vw;
+    z-index: -1; /* Pastikan dekorasi ada di bawah elemen utama */
+  }
+  
+  .decor-top {
+    top: 0;
+    height: 75px; /* Sesuaikan tinggi sesuai dekorasi */
+    background: url('@/assets/decor-feed-top.png') center center repeat-x;
+    background-size: cover;
+  }
+  
+  .decor-bottom {
+    bottom: 0;
+    height: 100px; /* Sesuaikan tinggi sesuai dekorasi */
+    background: url('@/assets/decor-feed-bottom.png') center center repeat-x;
+    background-size: cover;
+  }
+  
+  .content-container {
+    z-index: 0; /* Pastikan konten berada di atas dekorasi */
+    margin-top: 25px; /* Berikan jarak sehingga konten tidak tertutup dekorasi atas */
+    margin-bottom: 150px; /* Berikan jarak sehingga konten tidak tertutup dekorasi bawah */
+  }
+  </style>
+  
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,23 +1,68 @@
 <template>
-  <header class="bg-base-200 p-4 text-center">
-    <h1 class="text-3xl font-bold">Feed</h1>
-    <div class="border-t-4 border-[#006859] w-[4.5%] mx-auto mb-12 mt-6"></div>
-  </header>
-  <Feed />
-  <footer class="bg-base-200 p-4 text-center">
-    <div class="text-center">
-      <p>&copy; 2023 Your Company. All rights reserved.</p>
+    <DecorFeed>
+    <div class="w-[10vw]">
+      <!-- Breadcrumb -->
+      <div class="text-sm italic font-light flex justify-center mt-5 text-[#2D9596]">
+        <ul class="flex">
+          <li v-for="(crumb, index) in breadcrumbs" :key="index" class="flex items-center">
+            <a v-if="crumb.url" :href="crumb.url">{{ crumb.text }}</a>
+            <span v-else>{{ crumb.text }}</span>
+            <span v-if="index < breadcrumbs.length - 1" class="mx-2">/</span>
+          </li>
+        </ul>
+      </div>
+      <div class="text-4xl text-center mt-2 font-bold">Feed</div>
+      <div class="border-t-4 border-[#006859] w-[55%] mx-auto mb-2 mt-6"></div>
+      <section class="flex flex-col lg:flex-row items-start justify-center py-2 px-4">
+        <div class="text-center mb-8"></div>
+        <div class="flex flex-col lg:flex-row items-start justify-center w-full">
+          <aside class="w-full lg:w-[18%] p-2">
+            <Sidebar @changeContent="changeContent" />
+          </aside>
+          <main class="w-full lg:w-[75%] p-2">
+            <component :is="currentComponent" />
+          </main>
+        </div>
+      </section>
     </div>
-  </footer>
+  </DecorFeed>
 </template>
 
 <script>
+import { ref } from 'vue'
 import Feed from '../components/feed/Feed.vue'
+import DecorFeed from '../components/feed/DecorFeed.vue'
 
 export default {
   name: 'FeedView',
   components: {
-    Feed
+    Feed,
+    DecorFeed
+  },
+  setup() {
+    const currentComponent = ref('Feed')
+    const breadcrumbs = ref([{ text: 'Feed', url: '/feed' }, { text: '' }])
+
+    const changeContent = (componentName) => {
+      currentComponent.value = componentName
+      updateBreadcrumbs(componentName)
+    }
+
+    const updateBreadcrumbs = (componentName) => {
+      const breadcrumbMap = {
+        DetailBerita: 'Detail Berita'
+      }
+      breadcrumbs.value = [
+        { text: 'Feed', url: '/feed-detail' },
+        { text: breadcrumbMap[componentName] }
+      ]
+    }
+
+    return {
+      currentComponent,
+      changeContent,
+      breadcrumbs
+    }
   }
 }
 </script>
